d7b0ae4c902671586f571e2e246a2a01f747c58d
Hubert <hubertgancarczyk04@gmail.com>
2023-05-01 18:47:40 +0200

    feat(global): add route for getting single product, create ProductDetails page

--- frontend/src/screens/ProductDetails/ProductDetails.tsx
+++ frontend/src/screens/ProductDetails/ProductDetails.tsx
@@ -1,12 +1,73 @@
 import React from 'react';
+import {
+	Text,
+	Group,
+	Button,
+	Flex,
+	Container,
+	Loader,
+	Title,
+	Rating
+} from '@mantine/core';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
+import { useStyles } from './styles';
+import { ProductType } from '../../common/types';
 import { useParams } from 'react-router-dom';
+import { useCollection } from '../../hooks/useCollection';
 
 export function ProductDetails() {
+	const { classes } = useStyles();
 	const { _id } = useParams();
 
+	const { documents: product } = useCollection(`/products/${_id}`) as {
+		documents: ProductType;
+	};
+
+	if (!product) {
+		return (
+			<Flex justify="center">
+				<Loader color="indigo" mt={30} />
+			</Flex>
+		);
+	}
+
+	// TO DO
+	const handleAddToCart = () => {
+		console.log('Add to cart');
+	};
+
 	return (
-		<div>
-			<p>Product page id: {_id}</p>
-		</div>
+		<Container size="lg">
+			<Flex className={classes.space}>
+				<Container className={classes.carousel} fluid>
+					<Carousel
+						dynamicHeight
+						showArrows
+						showIndicators
+						showThumbs={false}
+					>
+						{product.imageUrls.map((image) => (
+							<div key={image}>
+								<img src={image} alt='product' height={600} />
+							</div>
+						))}
+					</Carousel>
+				</Container>
+				<div className={classes.informations}>
+					<Title fw={600} className={classes.title} order={3} size="h1">{product.brand}</Title>
+					<Text className={classes.name}>{product.name}</Text>
+					<Group>
+						<Text className={classes.price}>{product.price} zł</Text>
+						<Text className={classes.span}>w tym VAT</Text>
+					</Group>
+					<Group className={classes.rating} position="center">
+						<Rating size='32px' value={product.rating / product.reviewsNumber} fractions={2} readOnly />
+						<Text className={classes.reviewsNumber}>{product.reviewsNumber}</Text>
+					</Group>
+					<Button onClick={handleAddToCart} mih={48} fullWidth>Dodaj do koszyka</Button>
+				</div>
+			</Flex>
+		</Container>
 	);
 }
---
+++ frontend/src/screens/ProductDetails/styles.tsx
@@ -0,0 +1,66 @@
+import {
+	createStyles,
+	rem
+} from '@mantine/core';
+
+export const useStyles = createStyles((theme) => ({
+	carousel: {
+		flexBasis: '50%',
+		[theme.fn.smallerThan('sm')]: {
+			flexBasis: '100%'
+		}
+	},
+
+	space: {
+		paddingBlock: 40,
+		[theme.fn.smallerThan('sm')]: {
+			flexBasis: '100%',
+			flexDirection: 'column'
+		}
+	},
+
+	informations: {
+		flexBasis: '50%',
+		paddingLeft: '10%',
+		paddingRight: '4%',
+		marginTop: rem(16),
+		[theme.fn.smallerThan('sm')]: {
+			flexBasis: '100%'
+		}
+	},
+
+	title: {
+		fontFamily: 'serif'
+	},
+
+	name: {
+		fontFamily: 'sans-serif',
+		fontSize: rem(24),
+		fontWeight: 'bold'
+	},
+
+	price: {
+		fontSize: rem(24),
+		fontWeight: 'bold',
+		color: '#de000c'
+	},
+
+	span: {
+		fontSize: '12px',
+		fontWeight: 600,
+		color: '#d7d8da',
+		marginTop: rem(6)
+	},
+
+	rating: {
+		display: 'flex',
+		justifyContent: 'flex-start',
+		marginBlock: rem(60)
+	},
+
+	reviewsNumber: {
+		fontSize: rem(28),
+		fontWeight: 600,
+		marginLeft: '16px'
+	}
+}));
